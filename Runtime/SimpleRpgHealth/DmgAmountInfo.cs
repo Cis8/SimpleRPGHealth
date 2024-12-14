a3639a4267f98d104e70db0c9f4efe5d63368c27
Cis8 <ema.cis8@gmail.com>
2024-12-14 13:55:02 +0100

    Add docs to DmgAmountInfo.cs

--- Runtime/SimpleRpgHealth/DmgAmountInfo.cs
+++ Runtime/SimpleRpgHealth/DmgAmountInfo.cs
@@ -9,12 +9,24 @@ namespace ElectricDrill.SimpleRpgHealth
             NetAmount = netAmount;
         }
 
+        /// <summary>
+        /// Gets the initial damage amount.
+        /// </summary>
         public long RawAmount { get; }
-        
+
+        /// <summary>
+        /// Gets the damage amount after being reduced by the entity's defense.
+        /// </summary>
         public long DefReducedAmount { get; }
-        
+
+        /// <summary>
+        /// Gets the damage amount after being reduced by the entity's defense and barriers.
+        /// </summary>
         public long DefBarrierReducedAmount { get; }
-        
+
+        /// <summary>
+        /// Gets the actual amount of damage taken by the entity.
+        /// </summary>
         public long NetAmount { get; }
     }
 }
