f4c586d1b8b23cb46ca64b269c627b6ab55661fe
Cis8 <ema.cis8@gmail.com>
2024-12-25 16:31:16 +0100

    Change brackets

--- Runtime/SimpleRpgHealth/Death/DestroyOnDeathStrategy.cs
+++ Runtime/SimpleRpgHealth/Death/DestroyOnDeathStrategy.cs
@@ -5,8 +5,7 @@ namespace ElectricDrill.SimpleRpgHealth
     [CreateAssetMenu(fileName = "Destroy On Death Strategy", menuName = "Simple RPG Health/Death strategies/Destroy")]
     public class DestroyOnDeathStrategy : OnDeathStrategy
     {
-        public override void Die(EntityHealth entityHealth)
-        {
+        public override void Die(EntityHealth entityHealth) {
             Destroy(entityHealth.gameObject);
         }
     }
